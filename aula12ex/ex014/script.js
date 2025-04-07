1d2f61bd039c253fa70ed3e92ac6820599170f6e
Beatriz Nogueira <157432358+Benoggs@users.noreply.github.com>
2025-04-07 20:12:32 -0300

    exercicio 014

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = document.getElementById('msg');
+    var foto = document.getElementById('foto');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas.`;
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        foto.innerHTML = '<img id= "imagem" src= "fotomanha.png">'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        foto.innerHTML = '<img id= "imagem" src= "fototarde.png">'
+        document.body.style.background = '#b9846f'
+    } else {
+        // BOA NOITE!
+        foto.innerHTML = '<img id= "imagem" src= "fotonoite.png">'
+        document.body.style.background = '#515154'
+    }
+}
