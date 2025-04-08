d97a6a76b347f9c4d449c9bd65671d4e16df336d
Beatriz Nogueira <157432358+Benoggs@users.noreply.github.com>
2025-04-08 18:07:23 -0300

    bugs do código

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,19 +1,19 @@
 function carregar() {
-    var msg = document.getElementById('msg');
-    var foto = document.getElementById('foto');
-    var data = new Date();
-    var hora = data.getHours();
-    msg.innerHTML = `Agora são ${hora} horas.`;
+    var msg = document.getElementById('msg')
+    var foto = document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        // BOM DIA!
+        //BOM DIA!
         foto.innerHTML = '<img id= "imagem" src= "fotomanha.png">'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
-        // BOA TARDE!
+        //BOA TARDE!
         foto.innerHTML = '<img id= "imagem" src= "fototarde.png">'
         document.body.style.background = '#b9846f'
     } else {
-        // BOA NOITE!
+        //BOA NOITE!
         foto.innerHTML = '<img id= "imagem" src= "fotonoite.png">'
         document.body.style.background = '#515154'
     }
